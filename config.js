e8f12dfe996ad322aaac77dcfe0951c069d497a8
r1cardo42 <aleksanimator67@gmail.com>
2026-08-15 05:57:11 +0500

    Mega Alpha List
    ну чет навайбкодил минимально

---
+++ config.js
@@ -0,0 +1,4 @@
+// Вставьте сюда данные вашего проекта Supabase
+// Project Settings -> API -> Project URL / anon public key
+const SUPABASE_URL = "https://bktbxspankyryhohgfyw.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_ACdohKG5AOv4e4n0lQq6vA_8u7nSGCQ";
