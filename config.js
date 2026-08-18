7ea96990040ff397425d881cdc30d6e3950a107b
r1cardo42 <aleksanimator67@gmail.com>
2026-08-19 04:58:10 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Вставьте сюда данные вашего проекта Supabase
 // Project Settings -> API -> Project URL / anon public key
-window.SUPABASE_URL = "https://bktbxspankyryhohgfyw.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_ACdohKG5AOv4e4n01Qq6vA_8u7nSGCQ";
+const SUPABASE_URL = "https://bktbxspankyryhohgfyw.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_ACdohKG5AOv4e4n01Qq6vA_8u7nSGCQ";
