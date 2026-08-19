0dae3c64c2bdc7437005cddf138b12f965d3b780
r1cardo42 <aleksanimator67@gmail.com>
2026-08-19 05:14:20 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL // или import.meta.env для Vite
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = https://bktbxspankyryhohgfyw.supabase.co/rest/v1/
+const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrdGJ4c3Bhbmt5cnlob2hnZnl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2NTQ4MzcsImV4cCI6MjEwMjIzMDgzN30.W6v-BRgfJsBwPovnI3fzNBsOPv1XUQwmnrq1PBQPzv4
 
 // Можно добавить console.log(supabaseAnonKey) временно, 
 // чтобы убедиться, что ключ реально доходит до кода, а не выдает undefined.
