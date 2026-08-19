2b77afba0954370259009bcd539654d090ec6bf3
r1cardo42 <aleksanimator67@gmail.com>
2026-08-19 05:14:31 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-import { createClient } from '@supabase/supabase-js'
-
 const supabaseUrl = https://bktbxspankyryhohgfyw.supabase.co/rest/v1/
 const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrdGJ4c3Bhbmt5cnlob2hnZnl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2NTQ4MzcsImV4cCI6MjEwMjIzMDgzN30.W6v-BRgfJsBwPovnI3fzNBsOPv1XUQwmnrq1PBQPzv4
 
